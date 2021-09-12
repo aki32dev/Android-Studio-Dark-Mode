2f4776665a3d66d2942963cfbb503ea1ea49e71e
Aki32Dev <abdulkharisismail97@gmail.com>
2021-09-12 21:26:39 +0700

    Dark Mode

--- app/src/main/java/com/example/darkmode/MainActivity.java
+++ app/src/main/java/com/example/darkmode/MainActivity.java
@@ -1,16 +1,36 @@
 package com.example.darkmode;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-    Integer data;
+    private Button btChange;
+    private Boolean stateDark = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        data = 10;
+
+        btChange = findViewById(R.id.btChange);
+
+        btChange.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(!stateDark){
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+                    stateDark = true;
+                }
+                else{
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+                    stateDark = false;
+                }
+
+            }
+        });
     }
 }
